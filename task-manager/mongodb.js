4493da7bba7a178a6281cfb469bf91006de34129
mac <mac@Blossom-MacBook-Air.local>
2023-04-10 21:54:13 +0100

    new commit

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,9 +14,49 @@ MongoClient.connect(
       return console.log(`Unable to connect to database`);
     }
    const db = client.db(databaseName)
-   db.collection('users').insertOne({
-    name: 'Blossom',
-    age: 17
-   })
-  }
-);
+//    db.collection('users').insertOne({
+//     name: 'Blossom',
+//     age: 17
+//    }, (error, result) => {
+//  if (error) {
+//     return console.log(`Unable to insert user`);
+//  }
+//  console.log(result.ops);
+//    })
+
+// db.collection('users').insertMany([
+//  {
+//   name: 'Jane',
+//   age: 27
+// }, {
+//   name: 'Page',
+//   age: 10
+// }
+// ], (error, result) => {
+//  if(error){
+//   return console.log(`Unable to insert user`);
+//  }
+//  console.log(result.ops);
+// })
+
+db.collection('tasks').insertMany([
+  {
+    name: 'Happy Times',
+    description: 'These are the best times',
+    completed: true
+  },   {
+    name: "Leader",
+    description: 'Love Letter',
+    completed: false 
+  },   {
+    name: "Joyful",
+    description: 'Love Letter',
+    completed: true
+  }, 
+], (error, result)=>{
+if(error){
+  console.log(`Unable to insert tasks!`);
+}
+console.log(result.ops);
+})
+});
