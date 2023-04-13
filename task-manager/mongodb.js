14429ae35c75682a7dbe4e2a8784f6f12669d744
mac <blossomese@gmail.com>
2023-04-13 10:41:28 +0100

    new commit

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,12 @@
 // CRUD create read update delete
 
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectID()
+console.log(id);
 
 MongoClient.connect(
   connectionURL,
@@ -14,30 +16,30 @@ MongoClient.connect(
       return console.log(`Unable to connect to database`);
     }
    const db = client.db(databaseName)
-//    db.collection('users').insertOne({
-//     name: 'Blossom',
-//     age: 17
-//    }, (error, result) => {
-//  if (error) {
-//     return console.log(`Unable to insert user`);
-//  }
-//  console.log(result.ops);
-//    })
+   db.collection('users').insertOne({
+    name: 'Blossom',
+    age: 17
+   }, (error, result) => {
+ if (error) {
+    return console.log(`Unable to insert user`);
+ }
+ console.log(result.ops);
+   })
 
-// db.collection('users').insertMany([
-//  {
-//   name: 'Jane',
-//   age: 27
-// }, {
-//   name: 'Page',
-//   age: 10
-// }
-// ], (error, result) => {
-//  if(error){
-//   return console.log(`Unable to insert user`);
-//  }
-//  console.log(result.ops);
-// })
+db.collection('users').insertMany([
+ {
+  name: 'Jane',
+  age: 27
+}, {
+  name: 'Page',
+  age: 10
+}
+], (error, result) => {
+ if(error){
+  return console.log(`Unable to insert user`);
+ }
+ console.log(result.ops);
+})
 
 db.collection('tasks').insertMany([
   {
